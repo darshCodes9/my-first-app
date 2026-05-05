2a3e37f5021e65df1307c1bc95dbcc1389775c6a
Dhaval Rabari <darsh.git.7@gmail.com>
2026-05-05 10:43:04 +0530

    First commit after pipeline

--- index.test.js
+++ index.test.js
@@ -16,7 +16,7 @@ describe("Home Route", function() {
     expect(response.status).toBe(200);
 
     // Check the response contains our message
-    expect(response.body.message).toBe("I broke this on purpose");
+    expect(response.body.message).toBe("My first fullstack app");
 
     // Check the status field says "running"
     expect(response.body.status).toBe("running");
